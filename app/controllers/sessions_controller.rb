6d60830048e55cfdf279e1d69e8b40f568f59e9d
Ben Moelis <ben.moelis@kellogg.northwestern.edu>
2025-03-11 03:35:59 +0000

    Implemented user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+  
+    if @user && BCrypt::Password.new(@user["password"]) == params["password"]
+      session["user_id"] = @user["id"]
+      flash["notice"] = "You've logged in successfully."
+      redirect_to "/places"
+    else
+      flash["notice"] = "Invalid email or password."
+      redirect_to "/login"
+    end
   end
+  
 
   def destroy
   end
